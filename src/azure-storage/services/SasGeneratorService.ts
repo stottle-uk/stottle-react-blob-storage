fa4c770d867b20338b90f1c22cca545e6682825f
Stuart Tottle <stuart.tottle@ldn-m-0013.home>
2019-11-07 18:03:41 +0000

    added endpoint for sas token generator

--- src/azure-storage/services/SasGeneratorService.ts
+++ src/azure-storage/services/SasGeneratorService.ts
@@ -1,12 +1,12 @@
-import { Observable, of } from 'rxjs';
+import { Axios } from 'axios-observable';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { BlobStorageRequest } from '../types/azure-storage';
 
 export class SasGeneratorService {
   getSasToken(): Observable<BlobStorageRequest> {
-    return of({
-      storageUri: 'stottleblobstorage',
-      storageAccessToken:
-        'sv=2019-02-02&ss=b&srt=sco&sp=rwdlac&se=2019-11-14T19:20:59Z&st=2019-11-01T11:20:59Z&spr=https&sig=dG1zvo67VNDrqeWlrcrlz78W2ONVbYn2FlGJeFDifGg%3D'
-    });
+    return Axios.get<BlobStorageRequest>(
+      'https://stottle-generate-sas-token.azurewebsites.net/api/GenerateSasToken'
+    ).pipe(map(res => res.data));
   }
 }
